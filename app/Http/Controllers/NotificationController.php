00f502ad9bf0147295dd88c3bbd0b82d2a1e6dd0
sbenitez <sbenitez.js@gmail.com>
2021-10-27 16:17:01 -0300

    fix: ip

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -12,8 +12,10 @@ class NotificationController extends Controller
 
         $HTTP_X_FORWARDED_FOR = !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : null;
 
+        $HTTP_CLIENT_IP = !empty($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : null;
+
         Notification::create([
-            'url' => 'REMOTE_ADDR: ' . $_SERVER['REMOTE_ADDR']. ' - HTTP_CLIENT_IP: ' . $_SERVER['HTTP_CLIENT_IP'] . ' - HTTP_X_FORWARDED_FOR: ' . $HTTP_X_FORWARDED_FOR . ' - query string: ' .  $request->getQueryString(),
+            'url' => 'REMOTE_ADDR: ' . $_SERVER['REMOTE_ADDR']. ' - HTTP_CLIENT_IP: ' . $HTTP_CLIENT_IP . ' - HTTP_X_FORWARDED_FOR: ' . $HTTP_X_FORWARDED_FOR . ' - query string: ' .  $request->getQueryString(),
             'json' => json_encode($request->post())
         ]);
 
