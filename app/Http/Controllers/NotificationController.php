0ad1a8051b528b3af718d75f91e3ca7438a3f63a
sbenitez <sbenitez.js@gmail.com>
2021-10-27 16:18:50 -0300

    fix: ip

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -10,9 +10,9 @@ class NotificationController extends Controller
 {
     public function index(Request $request) {
 
-        $HTTP_X_FORWARDED_FOR = !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : null;
+        $HTTP_X_FORWARDED_FOR = !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : 'NO';
 
-        $HTTP_CLIENT_IP = !empty($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : null;
+        $HTTP_CLIENT_IP = !empty($_SERVER['HTTP_CLIENT_IP']) ? $_SERVER['HTTP_CLIENT_IP'] : 'NO';
 
         Notification::create([
             'url' => 'REMOTE_ADDR: ' . $_SERVER['REMOTE_ADDR']. ' - HTTP_CLIENT_IP: ' . $HTTP_CLIENT_IP . ' - HTTP_X_FORWARDED_FOR: ' . $HTTP_X_FORWARDED_FOR . ' - query string: ' .  $request->getQueryString(),
